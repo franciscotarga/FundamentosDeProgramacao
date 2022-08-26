b06a235b259eb89ed4db486409b4ad5688f690c5
Francisco Targa <111167953+franciscotarga@users.noreply.github.com>
2022-08-26 20:38:37 -0300

    Achar maior em Python
    Exercício realizado em aula para achar o maior valor, porém feito em python, utilizando inputs em array.

---
+++ Lista2/AcharMaiorEmPython.py
@@ -0,0 +1,9 @@
+arr = []
+
+v1 = arr.append(int(input("digite v1")))
+v2 = arr.append(int(input("digite v2")))
+v3 = arr.append(int(input("digite v3")))
+
+print("\n", arr)
+ans = max(arr)
+print("\nO maior valor é ", ans);
